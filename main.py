399e63b95e1acefd3c3fdd264c19546b9f182890
Дмитрий Песков <117769513+Dmitry-Peskov@users.noreply.github.com>
2022-11-21 21:33:25 +0300

    the sorting function is implemented

--- main.py
+++ main.py
@@ -9,23 +9,32 @@ folders_and_extensions = {'Видео': ['mp4', 'mov', 'avi', 'mkv', 'wmv', '3gp
 current_directory = os.getcwd()
 
 def create_folders(path: str, folders_name: dict) -> None:
-    '''Создаёт папки, в которые будут сортироваться файлы'''
+    '''Создаём папки, в которые будут сортироваться файлы'''
     for folder in folders_name.keys():
         if not os.path.exists(fr'{path}\{folder}'):
             os.mkdir(fr'{path}\{folder}')
 
-def create_paths(path: str, folders_name: dict) -> dict:
-    '''Создаём пути к папкам, в которые будут сортироваться файлы'''
-    path_list = dict()
-    for folder in folders_name.keys():
-        path_list[folder] = fr'{path}\{folder}'
-    return path_list
+def get_paths_file(path:str) -> list:
+    '''Собираем пути к файлам, которые будут отсортированны'''
+    file_list = [file.path for file in os.scandir(path) if not file.is_dir()]
+    return file_list
 
+def sort_files(path: str):
+    '''Производим сортировку файлов по папкам'''
+    file_paths = get_paths_file(path)
+    ext_list = list(folders_and_extensions.items())
+    for file_path in file_paths:
+        extension = file_path.split('.')[-1]
+        file_name = file_path.split('\\')[-1]
+        for dict_key_int in range(len(ext_list)):
+            if extension in ext_list[dict_key_int][1]:
+                os.rename(file_path, f'{current_directory}\\{ext_list[dict_key_int][0]}\\{file_name}')
+                print(f'"{file_name}" скопирован в папку "{ext_list[dict_key_int][0]}"\n')
 
 def main():
-    #create_folders(current_directory, folders_and_extensions)
-    #create_paths(current_directory, folders_and_extensions))
-    pass
+    create_folders(current_directory, folders_and_extensions)
+    sort_files(current_directory)
+    input('Сортировка окончена\nДля завершения нажмити "Enter"')
 
 if __name__ == "__main__":
     main()
